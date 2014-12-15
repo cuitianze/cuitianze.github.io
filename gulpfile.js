d8d63f7faede750ee00c184579b1ca268acf9575
cuitianze <cuitianze123@gmail.com>
2014-12-16 00:25:17 +0800

    12-15深夜Bug版首页提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 /**
  * Created by bluesky on 14-12-15.
  */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('scss', function () {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
--- js/index.js
+++ js/index.js
@@ -2,30 +2,34 @@
 2014-12-15
 cuitianze
 */
-// window.onload = function () {
-// 	var uMenu = document.getElementById("mainMenu");
-// 	var aLink = uMenu.getElementsByTagName("a");
-// 	var aWidth = [];
 
-// 	for (var i = 0; i < aLink.length; i++) {
-// 		aWidth.push( aLink[i].getElementsByTagName("img")[0].offsetWidth );
-// 		aLink[i].getElementsByTagName("img")[0].width = parseInt( aLink[i].getElementsByTagName("img")[0].offsetWidth / 2 );
-// 	};
-
-// 	uMenu.onmousemove = function () {
-// 		var event = event || window.event;
-// 		for (var i = 0; i < aLink.length; i++) {
-// 			var a = event.clientX - aLink[i].getElementsByTagName("img")[0].offsetLeft - aLink[i].getElementsByTagName("img")[0].offsetWidth / 2;
-// 			var b = event.clientY - aLink[i].getElementsByTagName("img")[0].offsetTop - aLink[i].getElementsByTagName("img")[0].offsetHeight /2;
-// 			var iScale = 1 - Math.sqrt( a*a + b*b )/300;
-// 			if(iScale<0.5) {
-// 				iScale = 0.5;
-// 			}
-// 			aLink[i].getElementsByTagName("img")[0].width = aWidth[i] * iScale;
-// 		};
-// 	}
-
-// }
-
+window.onload = function () {
+    var oMenu = document.getElementById("mainMenu");
+    var aMenu = oMenu.getElementsByTagName("a");
+    console.log(aMenu);
+    var aWidth = [], aHeight = [];
+    var i = 0;
+    //保存原宽度,设置当前宽度
+    for (i = 0; i < aMenu.length; i++) {
+        aWidth.push(aMenu[i].offsetWidth);
+        aHeight.push(aMenu[i].offsetHeight);
+        aMenu[i].style.width = parseInt(aMenu[i].offsetWidth / 2);
+        aMenu[i].style.height = parseInt(aMenu[i].offsetHeight / 2);
+        console.log(aMenu[i].style.width);
+    }
+    //鼠标移动事件
+    document.onmousemove = function (event) {
+        console.log(event);
+        var event = event || window.event;
+        for (i = 0; i < aMenu.length; i++)	{
+            var a = event.clientX - aMenu[i].offsetLeft - aMenu[i].offsetWidth / 2;
+            var b = event.clientY - aMenu[i].offsetTop - oMenu.offsetTop - aMenu[i].offsetHeight / 2;
+            var iScale = 1 - Math.sqrt(a * a + b * b) / 300;
+            if (iScale < 0.5) iScale = 0.5;
+            aMenu[i].style.width = aWidth[i] * iScale;
+            aMenu[i].style.height = aHeight[i] * iScale;
+        }
+    };
+};
 
 
